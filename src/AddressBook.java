3ff194c56ec7e6ace8b336fc4a3622db4691f5d6
marinaiskander <marinalatif@cmail.carleton.ca>
2023-09-19 15:08:57 -0400

    I added some function call to AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -21,6 +21,7 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        System.out.println("BuddyInfo");
         BuddyInfo buddy = new BuddyInfo("Marina","613","carleton");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
