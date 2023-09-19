1b4cab6545bbdc2c167e62d263b8e0e1cfa4ce8e
marinaiskander <marinalatif@cmail.carleton.ca>
2023-09-19 14:53:24 -0400

    I added some function call to AddressBook.java

---
+++ src/AddressBook.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<BuddyInfo> buddyInfo;
+
+    public AddressBook() {
+        buddyInfo = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if(buddy != null)
+        buddyInfo.add(buddy);
+    }
+
+    public void removeBuddy(int position) {
+        if (position >= 0 && position < buddyInfo.size()) {
+            buddyInfo.remove(position);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Marina","613","carleton");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
+    }
+}
