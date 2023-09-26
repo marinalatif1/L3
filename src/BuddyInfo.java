d0927d2aa349a37a443caa5dd621b99ca9f8174e
marinaiskander <marinalatif@cmail.carleton.ca>
2023-09-26 14:26:38 -0400

    Commited all of the files

---
+++ src/BuddyInfo.java
@@ -0,0 +1,31 @@
+public class BuddyInfo {
+    private String name;
+    private String number;
+    private String address;
+
+    public BuddyInfo(String name, String number, String address) {
+        this.name = name;
+        this.number = number;
+        this.address = address;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Marina","613","");
+        System.out.println("Hello " + buddy.getName());
+    }
+}
